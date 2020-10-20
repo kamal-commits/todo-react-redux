9d76eba799261b19dd6f2e9249d95904864f5834
Kamal Shah <kkamal714@gmail.com>
2020-10-20 15:38:53 +0530

    [Kamal] | Converts App function to Arrow function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import Todo from './pages/Todo'
+import { store } from './redux/store'
+import { Box, makeStyles } from '@material-ui/core'
+import './App.css'
+const useStyles = makeStyles((theme) => ({
+	root: {
+		// height: '100%'
+	}
+}))
 
-function App() {
-	return <div className='App'>Checking</div>
+const App = () => {
+	const classes = useStyles()
+	return (
+		<Provider store={store}>
+			<Box className={classes.root}>
+				<Todo />
+			</Box>
+		</Provider>
+	)
 }
 
 export default App
